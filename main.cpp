c641fef763fceac84e005bc9bc564fdd6ee9dc56
duanzhenjiang <duanzhenjiang0@gmail.com>
2024-03-19 12:26:01 +0800

    1

---
+++ main.cpp
@@ -0,0 +1,5 @@
+//
+// Created by duanzhenjiang on 2024/3/19.
+//
+
+#include "main.h"
---
+++ main.h
@@ -0,0 +1,14 @@
+//
+// Created by duanzhenjiang on 2024/3/19.
+//
+
+#ifndef CLION_MAIN_H
+#define CLION_MAIN_H
+
+
+class main {
+
+};
+
+
+#endif //CLION_MAIN_H
